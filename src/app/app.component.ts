f80cb037ff26132f0d2684cbec9c7fa6e0d96856
Burak Özel <burakk.oozel@gmail.com>
2022-08-21 13:49:29 +0300

    initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Products';
+export class AppComponent implements OnInit{
+
+  displayedColumns: string[] = ['productName', 'category','date','freshness', 'price', 'comment','action'];
+  dataSource !: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private dialog:MatDialog,private api:ApiService,private _snackBar: MatSnackBar){
+
+  }
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width:'30%'
+    }).afterClosed().subscribe(val => {
+      if(val === 'save'){
+        this.getAllProducts();
+      }
+    })
+  }
+  getAllProducts(){
+    this.api.getProduct().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        this._snackBar.open("Error while fetching the records");
+      }
+    });
+  }
+  editProduct(row:any){
+    this.dialog.open(DialogComponent,{
+      width:'30%',
+      data: row
+    }).afterClosed().subscribe(val => {
+      if(val === 'update'){
+        this.getAllProducts();
+      }
+    })
+  }
+  deleteProduct(id:number){
+    if(confirm("Are you sure to delete this product")){
+      this.api.deleteProduct(id).subscribe({
+        next: (res) => {
+          this._snackBar.open("Product deleted succesfully","Ok");
+          this.getAllProducts();
+        },
+        error: () => {
+          this._snackBar.open("Error while deleting the record!!","Ok");
+        }
+      });
+    }
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
